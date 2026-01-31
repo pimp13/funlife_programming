72ed5a392be5ade05847eeab74e15d8b23ab6072
pimp13 <pimp.puma.13@gmail.com>
2026-01-31 16:19:47 +0330

    create-set-in-go add set in golang example set in Python

---
+++ mini-apps/create-set-in-go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Set = map[string]struct{}
+
+func Add(s Set, v string) {
+	s[v] = struct{}{}
+}
+
+func Contains(s Set, v string) bool {
+	_, ok := s[v]
+	return ok
+}
+
+func main() {
+	newSet := Set{}
+	Add(newSet, "Pouya")
+	Add(newSet, "Ali")
+	Add(newSet, "Ali")
+
+	fmt.Println("my set", newSet)
+	fmt.Println("contains", Contains(newSet, "Ali"))
+
+	for k, _ := range newSet {
+		fmt.Println("My set is", k)
+	}
+}
