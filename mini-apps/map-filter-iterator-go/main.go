d62b7ae5816082096084a2e0609bd8531537f136
pimp13 <pimp.puma.13@gmail.com>
2026-01-31 13:33:10 +0330

    map-filter-iterator-go add collect and map dyn return type and static

--- mini-apps/map-filter-iterator-go/main.go
+++ mini-apps/map-filter-iterator-go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"iter"
 	"slices"
 )
@@ -13,18 +14,64 @@ func From[V any](src []V) *Iterator[V] {
 	return &Iterator[V]{seq: slices.Values(src)}
 }
 
+// Static return type return type Faghat mishe type iter
 func (it *Iterator[V]) Map(f func(V) V) *Iterator[V] {
 	return &Iterator[V]{
 		seq: func(yield func(V) bool) {
 			for v := range it.seq {
 				if !yield(f(v)) {
-					break
+					return
 				}
 			}
 		},
 	}
 }
 
-func main() {
+// Diynamic return type
+func Map[T, U any](it Iterator[T], f func(T) U) Iterator[U] {
+	return Iterator[U]{
+		seq: func(yield func(U) bool) {
+			for v := range it.seq {
+				if !yield(f(v)) {
+					return
+				}
+			}
+		},
+	}
+}
 
+func (it *Iterator[V]) Filter(pred func(V) bool) *Iterator[V] {
+	return &Iterator[V]{
+		seq: func(yield func(V) bool) {
+			for v := range it.seq {
+				if !pred(v) {
+					continue
+				}
+				if !yield(v) {
+					return
+				}
+			}
+		},
+	}
+}
+
+func (it *Iterator[V]) Collect() []V {
+	return slices.Collect(it.seq)
+}
+
+type User struct {
+	ID   uint
+	Name string
+}
+
+func main() {
+	myUser := []User{
+		{Name: "Pouya", ID: 1},
+		{Name: "Ali", ID: 2},
+		{Name: "Hassan", ID: 3},
+	}
+	result := From(myUser).
+		Map(func(u User) User { fmt.Printf("user: %s\n", u.Name); return u }).
+		Collect()
+	fmt.Println(result)
 }
