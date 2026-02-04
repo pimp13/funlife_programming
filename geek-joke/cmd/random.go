1997abea80fbc98afd3141a6eaaa29220260a79d
pimp13 <pimp.puma.13@gmail.com>
2026-02-04 17:25:07 +0330

    Add initial implementation of geek-joke CLI application; include module definition, command structure, and build setup. Implement random joke retrieval command with HTTP request handling.

---
+++ geek-joke/cmd/random.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2026 DevPouyaGh <pimp.puma.13@gmail.com>
+*/
+package cmd
+
+import (
+	"net/http"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Get a random geek joke",
+	Long:  `Get a random geek joke`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return executeRandomCmd(cmd, args)
+	},
+}
+
+// args: cmd *cobra.Command, args []string
+func executeRandomCmd(_ *cobra.Command, _ []string) error {
+	// fire off a request to get a random geek joke
+	url := "https://geek-jokes.sameerkumar.website/api?format=json"
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ geek-joke/main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2026 DevPouyaGh <pimp.puma.13@gmail.com>
+*/
+package main
+
+import "geek-joke/cmd"
+
+func main() {
+	cmd.Execute()
+}
