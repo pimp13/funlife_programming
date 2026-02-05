d9ec4d272d536ff0122b4af9d2ff43d0b907176e
pimp13 <pimp.puma.13@gmail.com>
2026-02-05 16:41:51 +0330

    Refactor main function in crack-safe-lock to simplify string comparison logic by using early continue statements, improving readability and maintainability.

--- mini-apps/crack-safe-lock/main.go
+++ mini-apps/crack-safe-lock/main.go
@@ -46,17 +46,23 @@ func sit(a, b string) (int, int) {
 func main() {
 	for i := 0; i < 1000; i++ {
 		strI := fmt.Sprintf("%03d", i)
-		if fix, found := sit(strI, "682"); fix == 1 && found == 0 {
-			if fix, found := sit(strI, "614"); fix == 0 && found == 1 {
-				if fix, found := sit(strI, "206"); fix == 0 && found == 2 {
-					if fix, found := sit(strI, "738"); fix == 0 && found == 0 {
-						if fix, found := sit(strI, "380"); fix == 0 && found == 1 {
-							fmt.Println(strI)
-						}
-					}
-				}
-			}
+		if fix, found := sit(strI, "682"); fix != 1 || found != 0 {
+			continue
 		}
+		if fix, found := sit(strI, "614"); fix != 0 || found != 1 {
+			continue
+		}
+		if fix, found := sit(strI, "206"); fix != 0 || found != 2 {
+			continue
+		}
+		if fix, found := sit(strI, "738"); fix != 0 || found != 0 {
+			continue
+		}
+		if fix, found := sit(strI, "380"); fix != 0 || found != 1 {
+			continue
+		}
+
+		fmt.Println(strI)
 
 	}
 }
