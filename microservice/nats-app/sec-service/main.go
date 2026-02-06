049af3b6ebe35fb2b6b2eaae3782a4c40d90f479
pimp13 <pimp.puma.13@gmail.com>
2026-02-06 19:52:22 +0330

    Add NATS subscription logic to sec-service; connect to NATS server, subscribe to "foo" subject, and handle message retrieval with error logging.

--- microservice/nats-app/sec-service/main.go
+++ microservice/nats-app/sec-service/main.go
@@ -1,6 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
-	
+	nc, err := nats.Connect("nats://localhost:4222")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	subj, err := nc.SubscribeSync("foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msg, err := subj.NextMsg(time.Duration(5 * time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Got message from first service:\n %s\n", msg.Data)
 }
