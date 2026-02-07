8286efe113ed378c1b527ce08d48da552cfcf9fe
pimp13 <pimp.puma.13@gmail.com>
2026-02-07 15:26:37 +0330

    Add release profile optimizations and comment out unused code in main.rs

---
+++ rust-apps/ilove-rust/target/release/deps/ilove_rust-22763d7edd970144.d
@@ -0,0 +1,5 @@
+/home/pouya/w/funlife_programming/rust-apps/ilove-rust/target/release/deps/ilove_rust-22763d7edd970144.d: src/main.rs
+
+/home/pouya/w/funlife_programming/rust-apps/ilove-rust/target/release/deps/ilove_rust-22763d7edd970144: src/main.rs
+
+src/main.rs:
---
+++ rust-apps/ilove-rust/target/release/deps/ilove_rust-29aabf60d37ace57.d
@@ -0,0 +1,5 @@
+/home/pouya/w/funlife_programming/rust-apps/ilove-rust/target/release/deps/ilove_rust-29aabf60d37ace57.d: src/main.rs
+
+/home/pouya/w/funlife_programming/rust-apps/ilove-rust/target/release/deps/ilove_rust-29aabf60d37ace57: src/main.rs
+
+src/main.rs:
