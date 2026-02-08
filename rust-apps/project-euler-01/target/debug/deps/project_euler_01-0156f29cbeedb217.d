70c045444ff7d14ca5a03817b86f72e2074af9f8
pimp13 <pimp.puma.13@gmail.com>
2026-02-08 14:17:37 +0330

    anjam project perisna word game and start project euler 01.

---
+++ rust-apps/project-euler-01/target/debug/deps/project_euler_01-0156f29cbeedb217.d
@@ -0,0 +1,5 @@
+/home/pouya/w/funlife_programming/rust-apps/project-euler-01/target/debug/deps/project_euler_01-0156f29cbeedb217.d: src/main.rs
+
+/home/pouya/w/funlife_programming/rust-apps/project-euler-01/target/debug/deps/libproject_euler_01-0156f29cbeedb217.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust-apps/project-euler-01/target/debug/deps/project_euler_01-06f9ffb37bfa0f11.d
@@ -0,0 +1,5 @@
+/home/pouya/w/funlife_programming/rust-apps/project-euler-01/target/debug/deps/project_euler_01-06f9ffb37bfa0f11.d: src/main.rs
+
+/home/pouya/w/funlife_programming/rust-apps/project-euler-01/target/debug/deps/libproject_euler_01-06f9ffb37bfa0f11.rmeta: src/main.rs
+
+src/main.rs:
