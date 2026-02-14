733a849730ab684bb18d2bc8e60ab524340aa816
pimp13 <pimp.puma.13@gmail.com>
2026-02-14 15:49:56 +0330

    Implement HTTP server for URL shortener with a ping endpoint, update Go module dependencies, and add go.sum file for dependency management

--- url_shortner_go/cmd/main.go
+++ url_shortner_go/cmd/main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const PORT = ":4224"
+
 func main() {
-	// this startpoint app
-	println("Hello world this is url and link shortner cli app :)")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	log.Printf("INF: server is running on port %s\n", PORT)
+	if err := r.Run(fmt.Sprintf("0.0.0.0%s", PORT)); err != nil {
+		log.Fatalf("server error: failed to run server: %s", err.Error())
+	}
 }
